14bba52ba9ccbf332eb4273f81f5f5bf4993454f
IvanOndok75 <kotikpusistik417@gmail.com>
2025-10-13 19:06:49 +0300

    a

---
+++ music/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h1>Retro Music Player</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+    background-color: #dd3489ff;
+    text-align: center;
+}
+h1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c0509ff;
+  margin: 20px;
+}
+
+</style>
