e8c31ed053554a304e1a4fc8565e33b78d6affed
IvanOndok75 <kotikpusistik417@gmail.com>
2025-10-17 16:48:04 +0300

    a

--- music/src/components/MyFooter.vue
+++ music/src/components/MyFooter.vue
@@ -17,14 +17,14 @@ export default {
   color: #2c0509ff;
   font-size: 20px;
   width: 100%;
-  padding: 10px 0;   /* невеликий внутрішній відступ для тексту */
+  padding: 10px 0; 
   margin: 0;
-  position: fixed;   /* "прилипає" до низу */
-  bottom: 0;         /* фіксується внизу */
+  position: fixed; 
+  bottom: 0;        
   left: 0;
 }
 
 .footer p {
-  margin: 0;         /* прибирає відступи у <p> */
+  margin: 0;       
 }
 </style>
